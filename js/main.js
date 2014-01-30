3adc6145851ddee76b5249421c63403eb68cc26d
dchapman <david@ronikdesign.com>
2014-01-29 22:03:02 -0500

    adding all html5 boilerplate files, index files, style, etc

---
+++ js/main.js
@@ -0,0 +1,276 @@
+var heartPieces = [
+    // lowest row of 3 panes left-right
+    [
+        {
+            baseFill : '#aa1215',
+            pulseFill : '#c25a3f',
+            basePoints : '407.29,658.408 350.092,568.685 267.098,578.218 	',
+            pulsePoints : '407.29,658.408 350.092,568.685 267.098,578.218 	'
+        },
+
+        {
+            baseFill : '#980c11',
+            pulseFill : '#b4553d',
+            basePoints : '487.631,568.685 407.29,658.408 350.092,568.685 	',
+            pulsePoints : '487.631,568.685 407.29,658.408 350.092,568.685 	'
+        },
+
+        {
+            baseFill : '#810202',
+            pulseFill : '#a54e33',
+            basePoints : '535.706,582.704 407.29,658.408 487.631,568.685 	',
+            pulsePoints : '535.706,582.704 407.29,658.408 487.631,568.685 	'
+        }
+    ],
+
+    // low row of 7 panes left-right
+    [
+        {
+            baseFill : '#bd141b',
+            pulseFill : '#ce5a44',
+            basePoints : '134.487,465.296 195.891,436.697 267.098,578.218 	',
+            pulsePoints : '134.487,465.296 195.891,436.697 267.098,578.218 	'
+        },
+
+        {
+            baseFill : '#c3181d',
+            pulseFill : '#d35d45',
+            basePoints : '267.098,578.218 195.891,436.697 350.092,568.685 	',
+            pulsePoints : '267.098,578.218 195.891,436.697 350.092,568.685 	'
+        },
+
+        {
+            baseFill : '#c1181f',
+            pulseFill : '#d25d47',
+            basePoints : '195.891,436.697 350.092,568.685 393.843,404.733 	',
+            pulsePoints : '195.891,436.697 350.092,568.685 393.843,404.733 	'
+        },
+
+        {
+            baseFill : '#af1117',
+            pulseFill : '#c45941',
+            basePoints : '350.092,568.685 393.843,404.733 487.631,568.685 	',
+            pulsePoints : '350.092,568.685 393.843,404.733 487.631,568.685 	'
+        },
+
+        {
+            baseFill : '#9e0c10',
+            pulseFill : '#b9553c',
+            basePoints : '393.843,404.733 487.631,568.685 581.42,410.761 	',
+            pulsePoints : '393.843,404.733 487.631,568.685 581.42,410.761 	'
+        },
+
+        {
+            baseFill : '#830001',
+            pulseFill : '#a64d32',
+            basePoints : '646.037,499.149 487.631,568.685 581.42,410.761 	',
+            pulsePoints : '646.037,499.149 487.631,568.685 581.42,410.761 	'
+        },
+
+        {
+            baseFill : '#740206',
+            pulseFill : '#9c4e35',
+            basePoints : '535.706,582.704 487.631,568.685 646.037,499.149 	',
+            pulsePoints : '535.706,582.704 487.631,568.685 646.037,499.149 	'
+        }
+    ],
+
+    // high row of 8 panes left-right
+    [
+        {
+            baseFill : '#ba3731',
+            pulseFill : '#cc7353',
+            basePoints : '195.891,436.697 134.487,465.296 100,347.535 	',
+            pulsePoints : '195.891,436.697 134.487,465.296 100,347.535 	'
+        },
+
+        {
+            baseFill : '#c93939',
+            pulseFill : '#d67559',
+            basePoints : '100,347.114 295.148,269.167 195.891,436.697 	',
+            pulsePoints : '100,347.114 295.148,269.167 195.891,436.697 	'
+        },
+
+        {
+            baseFill : '#db3e3e',
+            pulseFill : '#e4785c',
+            basePoints : '295.148,269.167 195.891,436.697 393.843,404.733 	',
+            pulsePoints : '295.148,269.167 195.891,436.697 393.843,404.733 	'
+        },
+
+        {
+            baseFill : '#d41c25',
+            pulseFill : '#de614a',
+            basePoints : '393.843,404.733 295.148,269.167 462.234,261.317 	',
+            pulsePoints : '393.843,404.733 295.148,269.167 462.234,261.317 	'
+        },
+
+        {
+            baseFill : '#a20d11',
+            pulseFill : '#bb563d',
+            basePoints : '462.234,261.317 393.843,404.733 581.42,410.761 	',
+            pulsePoints : '462.234,261.317 393.843,404.733 581.42,410.761 	'
+        },
+
+        {
+            baseFill : '#850203',
+            pulseFill : '#a84e33',
+            basePoints : '581.42,410.761 462.234,261.317 573.266,279.962 	',
+            pulsePoints : '581.42,410.761 462.234,261.317 573.266,279.962 	'
+        },
+
+        {
+            baseFill : '#710000',
+            pulseFill : '#994d31',
+            basePoints : '700,348.096 573.266,279.962 581.42,410.761 	',
+            pulsePoints : '700,348.096 573.266,279.962 581.42,410.761 	'
+        },
+
+        {
+            baseFill : '#610000',
+            pulseFill : '#8e4d31',
+            basePoints : '581.42,410.761 700,348.096 646.037,499.149 	',
+            pulsePoints : '581.42,410.761 700,348.096 646.037,499.149 	'
+        }
+    ],
+
+    // highest row of eight panes left-right
+    [
+        {
+            baseFill : '#c63936',
+            pulseFill : '#d47557',
+            basePoints : '160.563,210.847 100,347.535 100,347.254 295.148,269.167 		',
+            pulsePoints : '160.563,210.847 100,347.535 100,347.254 295.148,269.167 		'
+        },
+
+        {
+            baseFill : '#d61923',
+            pulseFill : '#e05e49',
+            basePoints : '160.563,210.847 267.389,141.873 295.148,269.167 		',
+            pulsePoints : '160.563,210.847 267.389,141.873 295.148,269.167 		'
+        },
+
+        {
+            baseFill : '#ba151c',
+            pulseFill : '#cd5c45',
+            basePoints : '295.148,269.167 267.389,141.873 401.133,219.259 		',
+            pulsePoints : '295.148,269.167 267.389,141.873 401.133,219.259 		'
+        },
+
+        {
+            baseFill : '#b4171c',
+            pulseFill : '#c95c45',
+            basePoints : '401.133,219.259 295.148,269.167 462.234,261.317 		',
+            pulsePoints : '401.133,219.259 295.148,269.167 462.234,261.317 		'
+        },
+
+        {
+            baseFill : '#c5171d',
+            pulseFill : '#d45c45',
+            basePoints : '462.234,261.317 401.133,219.259 534.27,141.592 		',
+            pulsePoints : '462.234,261.317 401.133,219.259 534.27,141.592 		'
+        },
+
+        {
+            baseFill : '#a30d11',
+            pulseFill : '#bc563d',
+            basePoints : '534.27,141.592 462.234,261.317 573.266,279.962 		',
+            pulsePoints : '534.27,141.592 462.234,261.317 573.266,279.962 		'
+        },
+
+        {
+            baseFill : '#7f0b07',
+            pulseFill : '#a35435',
+            basePoints : '573.244,279.962 534.27,141.592 639.414,211.548 		',
+            pulsePoints : '573.244,279.962 534.27,141.592 639.414,211.548 		'
+        },
+
+        {
+            baseFill : '#5c0102',
+            pulseFill : '#8b4e33',
+            basePoints : '573.266,279.962 639.414,211.548 700,348.096 		',
+            pulsePoints : '573.266,279.962 639.414,211.548 700,348.096 		'
+        }
+    ]
+];
+
+var heart = Snap("#heart");
+
+constructHeart();
+
+function constructHeart() {
+    console.log('constructing!');
+
+    for (var r=0; r < heartPieces.length; r++) {
+        var currentRow = heartPieces[r];
+
+        for (var p=0; p < currentRow.length; p++) {
+            var currentPolygon = currentRow[p],
+                id = 'polygon-' + r + '-' + p;
+
+            var snapPolygon = heart.el('polygon', {
+                fill: currentPolygon.baseFill,
+                points: currentPolygon.basePoints,
+                id: id,
+
+                baseFill: currentPolygon.baseFill,
+                pulseFill : currentPolygon.pulseFill,
+                basePoints: currentPolygon.basePoints,
+                pulsePoints: currentPolygon.pulsePoints
+            });
+
+            var animation = Snap.animation({
+                fill: '#ff0000'
+            }, 600);
+        }
+    }
+}
+
+/*function constructHeart() {
+    console.log('constructing!');
+
+    for (var r=0; r < heartPieces.length; r++) {
+        var currentRow = heartPieces[r],
+            snapGroup = heart.el('g', {
+                id: 'row-' + r
+            });
+
+        for (var p=0; p < currentRow.length; p++) {
+            var currentPolygon = currentRow[p],
+                snapPolygon = heart.el('polygon', {
+                    fill: currentPolygon.baseFill,
+                    points: currentPolygon.basePoints,
+                    id: 'polygon-' + r + '-' + p
+                });
+
+            snapPolygon.appendTo(snapGroup);
+        }
+    }
+}*/
+
+/*
+pulseHeart();
+
+function pulseHeart(heartPiece) {
+    for (var r=0; r < heart.selectAll('polygon').length; r++) {
+        var adjust = 3,
+            adjustedPointer = r + adjust,
+            selector = 'polygon:nth-child(' + adjustedPointer + ')',
+            piece = heart.select(selector),
+            baseFill = piece.attr('baseFill'),
+            pulseFill = piece.attr('pulseFill'),
+            basePoints = piece.attr('basePoints'),
+            pulsePoints = piece.attr('pulsePoints'),
+            delayIncrement = 20,
+            delay = 100 + (r * delayIncrement);
+
+        piece.animate({
+            fill: pulseFill
+        }, delay);
+
+        console.log(baseFill);
+        console.log(pulseFill);
+        console.log(basePoints);
+        console.log(pulsePoints);
+    }
+}*/
